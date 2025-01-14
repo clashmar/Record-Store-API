c48f25f5d2cd3d40768e01274aa3d9201083a8b0
Charlie Lashmar <charlie@lashmar.com>
2025-01-14 09:31:45 +0000

    Initial Commit

---
+++ RecordStoreAPI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RecordStoreAPI.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
