35fda7cdbebc0c1ac08a0087f412348ab2f58959
Charlie Lashmar <charlie@lashmar.com>
2025-01-23 11:18:20 +0000

    created Blazor project

--- RecordStoreAPI.Tests/obj/Debug/net8.0/RecordStoreAPI.Tests.AssemblyInfo.cs
+++ RecordStoreAPI.Tests/obj/Debug/net8.0/RecordStoreAPI.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecordStoreAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e44ed78a814c1fc4cacd2ee67b0f5b506bff7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b267ff3360ea78d2464c239cb4b3fb4fd3c64613")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecordStoreAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecordStoreAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
