c274ee7f65410eb0987cde9a5fa1ed531b99ee1b
Charlie Lashmar <charlie@lashmar.com>
2025-01-28 08:36:17 +0000

    formatted success notification to be consistent with error notification

--- RecordStoreFrontend/RecordStoreFrontend/obj/Debug/net8.0/RecordStoreFrontend.AssemblyInfo.cs
+++ RecordStoreFrontend/RecordStoreFrontend/obj/Debug/net8.0/RecordStoreFrontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecordStoreFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17589008bcd7186d31e43d602627d76e9464e4f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6655a342803069453d2fc3fd76a0b6c4583343a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecordStoreFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecordStoreFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
